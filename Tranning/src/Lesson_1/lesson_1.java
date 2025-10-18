0926c121131907f99ead537dbf5682138318b68b
Stanislav <133857994+Stanislav20000@users.noreply.github.com>
2025-10-18 12:05:05 +0300

    Add files via upload

---
+++ Tranning/src/Lesson_1/lesson_1.java
@@ -0,0 +1,16 @@
+/**
+ * Урок 1. Команда вывода информации в консоль
+ * */
+
+package Lesson_1;
+
+public class lesson_1 {
+
+    static void main(String[] args) {
+
+        // Команда вывода информации в консоль
+        System.out.println("Hello world!"); // Данная команда выводит в консоль "Hello world"
+
+    }
+
+}
